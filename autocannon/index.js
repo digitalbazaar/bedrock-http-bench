84e3af790935de2096d455aec57931f53439419f
Matthew Collier <collier.matthew@gmail.com>
2018-04-26 21:28:12 -0400

    Increase connections.

--- autocannon/index.js
+++ autocannon/index.js
@@ -126,7 +126,7 @@ autocannon({
   // url: 'https://bedrock.local:18443/post1',
   // url: 'https://bedrock.local:18443/post2',
   method: 'POST',
-  connections: 30, //default
+  connections: 50, //default
   pipelining: 1, // default
   duration: 60, // default
   // requests,
