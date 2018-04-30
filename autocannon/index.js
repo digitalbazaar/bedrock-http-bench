71abf5a697f33c3771e2d58ff7fa4ed850a76404
Matthew Collier <collier.matthew@gmail.com>
2018-04-30 11:16:09 -0400

    Reduce connections.

--- autocannon/index.js
+++ autocannon/index.js
@@ -159,7 +159,7 @@ autocannon({
   // url: 'https://bedrock.local:18443/post1',
   // url: 'https://bedrock.local:18443/post2',
   method: 'POST',
-  connections: 20, //default
+  connections: 10, //default
   pipelining: 1, // default
   duration: 30, // default
   // requests,
